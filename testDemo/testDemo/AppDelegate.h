f11d09aa5a1daa850733f677e79a4a3827993577
zhangguangkai <guangkai.zhang@jimubox.com>
2016-12-21 17:36:30 +0800

    testDemo

---
+++ testDemo/testDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo
+//
+//  Created by zgk on 2016/12/21.
+//  Copyright © 2016年 BBAE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testDemo/testDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testDemo
+//
+//  Created by zgk on 2016/12/21.
+//  Copyright © 2016年 BBAE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
