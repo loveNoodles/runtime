18a1bddfb4f740cd62763908e4932d3591fd8016
zhangguangkai <guangkai.zhang@jimubox.com>
2017-01-15 17:03:30 +0800

    method swizzling

---
+++ RuntimeSyntaxDemo/RuntimeSyntaxDemo/UIViewController+trace.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+trace.h
+//  RuntimeSyntaxDemo
+//
+//  Created by zgk on 2017/1/15.
+//  Copyright © 2017年 BBAE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (trace)
+
+@end
---
+++ RuntimeSyntaxDemo/RuntimeSyntaxDemo/UIViewController+trace.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+trace.m
+//  RuntimeSyntaxDemo
+//
+//  Created by zgk on 2017/1/15.
+//  Copyright © 2017年 BBAE. All rights reserved.
+//
+
+#import "UIViewController+trace.h"
+
+@implementation UIViewController (trace)
+
+@end
